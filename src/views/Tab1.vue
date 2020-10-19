5c960b2ac64ed7b7198ddc9bc0d22e2a07430ee6
scarfaceshing <edrianjansolatorio@gmail.com>
2020-10-19 14:10:11 +0800

    push

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -11,14 +11,13 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-     <ion-button @click="() => router.replace('/login')">Logout</ion-button>
       <ExploreContainer name="Tab 1 page" />
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
 import { useRouter } from 'vue-router';
 import { defineComponent } from 'vue';
@@ -42,7 +41,7 @@ const debugCall: any = {
 
 export default defineComponent({
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton },
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
   methods: debugCall,
   setup() {
    const router = useRouter();
